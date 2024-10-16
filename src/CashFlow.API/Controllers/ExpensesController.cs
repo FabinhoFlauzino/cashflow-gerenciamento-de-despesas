dc0dfaa3c3d204513e4598063c194f3978c6c92d
unknown <fabioflauzino@hotmail.com>
2024-10-16 00:43:42 -0300

    Criando conexão com banco de dados

--- src/CashFlow.API/Controllers/ExpensesController.cs
+++ src/CashFlow.API/Controllers/ExpensesController.cs
@@ -1,5 +1,6 @@
 ﻿using CashFlow.Application.UseCases.Expenses.Register;
 using CashFlow.Communication.Requests;
+using CashFlow.Communication.Responses;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CashFlow.API.Controllers;
@@ -19,12 +20,16 @@ public class ExpensesController : ControllerBase
             return Created(string.Empty, response);
         } 
         catch (ArgumentException ex) 
-        { 
-            return BadRequest(ex.Message);
+        {
+            var errorResponse = new ResponseErrorJson(ex.Message);
+
+            return BadRequest(errorResponse);
         }
         catch 
-        { 
-            return StatusCode(StatusCodes.Status500InternalServerError, "unknown error");
+        {
+            var errorResponse = new ResponseErrorJson("Unknown error");
+            
+            return StatusCode(StatusCodes.Status500InternalServerError, errorResponse);
         }
     }
 }
---
+++ src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
@@ -0,0 +1,28 @@
+﻿using CashFlow.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CashFlow.Infrastructure.DataAccess;
+
+public class CashFlowDbContext : DbContext
+{
+    public DbSet<Expense> Expenses { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        //string de conexão com os dados do banco
+        //Url do servidor
+        //Nome do banco de dados
+        //Usuário
+        //Senha
+        var connectionString = "Server=localhost;Database=cashflowdb;Uid=root;Pwd=root";
+
+        //versão do mySql
+        var version = new Version(8, 0, 28);
+
+        //instanciando uma nova versão
+        var serverVersion = new MySqlServerVersion(version);
+
+        //usando o mysql passando a string de conexão e a versão do banco
+        optionsBuilder.UseMySql(connectionString, serverVersion);
+    }
+}
