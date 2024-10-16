f2b9cc051b4e3d1be6b8fb1a6d7b04fe2bd5954b
Fabio Flauzino <fabio@mfrural.com.br>
2024-10-16 14:22:57 -0300

    merge

--- src/CashFlow.API/obj/Debug/net8.0/CashFlow.API.AssemblyInfo.cs
+++ src/CashFlow.API/obj/Debug/net8.0/CashFlow.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CashFlow.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cdff1fb5b2718b04965d69710b4b439ab19330f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c652f7ee665759856240c51641d4d56357e8b47")]
 [assembly: System.Reflection.AssemblyProductAttribute("CashFlow.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CashFlow.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
